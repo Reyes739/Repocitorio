ca51ffd60154980e29217984785d3b9d02352f4b
Reyes739 <josemanuereyes215@gmail.com>
2023-08-17 19:05:54 -0600

    Actividad 1

---
+++ READMI.md
@@ -0,0 +1,9 @@
+# Programacion oriantada a objetos y laboratorio 
+- Nombre: Jose Manuel Reyes Cuevas 
+- Correo: josemanuereyes215@gmail.com
+
+ ## Actividad 1 
+ - _p01_HolaMundo 
+ - _p02_AreaCircilo
+ - _p03_AreaTriangulo
+ - _p04_PagaTrabajador 
---
+++ _p04_PagaTrabajador.java
@@ -0,0 +1,31 @@
+
+//Calcilar la paga de un trabajador 
+import java.util.Scanner;
+
+public class _p04_PagaTrabajador {
+    public static void main(String[] args) {
+        String nombre;
+        int horas;
+        float paga, tasa, impuesto, pagabruta, paganeta;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Nombre del tabajador");
+        nombre = obj.nextLine();
+        System.out.print("Horas trabajadas?");
+        horas = obj.nextInt();
+        System.out.print("Paga x hora  ?");
+        paga = obj.nextFloat();
+        tasa = 0.3f;
+        pagabruta = horas * paga;
+        paganeta = pagabruta - impuesto;
+
+        System.out.println("\nResumen de Pago\n");
+        System.out.println(String.format(
+                "El trabajador %s, trabajo %d, horas, a un pago de %.2f pesos la hora, con una tasa de inpuesto de %.2f",
+                nombre, horas, paga, tasa));
+        System.out.println(String.format("Paga bruta      =%.2f", pagabruta));
+        System.out.println(String.format("Impuestos       =%.2f", impuesto));
+        System.out.println(String.format("Paga neta       =%.2f", paganeta));
+
+    }
+}
