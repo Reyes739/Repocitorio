85c044aa9bbf12b1d9765c030ccf054074fb22b9
Reyes739 <josemanuereyes215@gmail.com>
2023-08-17 21:49:38 -0600

    Hola

---
+++ _p02_AreaCircilo.java
@@ -0,0 +1,19 @@
+
+// Crear el area del circulo 
+import java.util.Scanner;
+
+public class _p02_AreaCircilo {
+    public static void main(String[] args) {
+        double radio, area;
+        Scanner lradio = new Scanner(System.in);
+
+        System.out.println("\nCalcular el area de un circulo");
+        System.out.print("\nDame el radio? ");
+        radio = lradio.nextFloat();
+
+        area = Math.PI * Math.pow(radio, 2);
+
+        System.out.println("El circulo de radio " + radio + " tiene una area de " + area);
+
+    }
+}
